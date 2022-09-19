0f0a4a1d07aafedea919e18d6968df51b062a8c2
mrshine4k <111043449+mrshine4k@users.noreply.github.com>
2022-09-19 13:22:48 +0700

    s

--- appServer.js
+++ appServer.js
@@ -1,5 +1,5 @@
 const  express = require("express");
-const PORT = process.env.port || 80;
+const PORT = process.env.port || 3000;
 const appserver = express();
 const router = express.Router();
 const fs = require("fs");
