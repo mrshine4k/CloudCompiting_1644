ead744ea2d3c3787e87f8196c02bc6ff8446adb6
mrshine4k <111043449+mrshine4k@users.noreply.github.com>
2022-09-19 13:29:41 +0700

    s

--- appserver.js
+++ appserver.js
@@ -1,20 +1,43 @@
-const  express = require("express");
-const PORT = process.env.port || 3000;
+const express = require('express');
 const appserver = express();
 const router = express.Router();
 const fs = require("fs");
 const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+console.log("Web has been initialized" +" "+ port);
 
 
 // ------------------- Routing
-router.get( "/" , (yeucau, trave) => {
+router.get('/',(request,response)=>
+{
     data = fs.readFileSync("./html/main.html");
     pageContent = data.toString();
-    trave.send(pageContent);
+    response.send(pageContent);
+});
+router.get('/home',(request,response)=>
+{
+    data = fs.readFileSync("./html/home.html");
+    pageContent = data.toString();
+    response.send(pageContent);
+});
+router.get('/about',(request,response)=>
+{
+    data = fs.readFileSync("./html/about.html");
+    pageContent = data.toString();
+    response.send(pageContent);
+});
+router.get('/contact',(request,response)=>
+{
+    data = fs.readFileSync("./html/contact.html");
+    pageContent = data.toString();
+    response.send(pageContent);
 });
 
 // -------------------------
-appserver.use("/", router);
+appserver.use('/', router);
+appserver.use('/home', router);
+appserver.use('/about', router);
+appserver.use('/contact', router);
 // ----------- RUN / Lauching !!! 
 appserver.listen( PORT,host );
 
