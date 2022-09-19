93ab44331781da18b94eae936e197e50244f6b5d
mrshine4k <111043449+mrshine4k@users.noreply.github.com>
2022-09-19 13:32:18 +0700

    s

--- appserver.js
+++ appserver.js
@@ -1,44 +1,39 @@
-const express = require('express');
+const express = require("express");
 const appserver = express();
 const router = express.Router();
 const fs = require("fs");
-const host = '0.0.0.0';
+const host = "0.0.0.0";
 const port = process.env.PORT || 3000;
-console.log("Web has been initialized" +" "+ port);
-
+console.log("Web has been initialized" + " " + port);
 
 // ------------------- Routing
-router.get('/',(request,response)=>
-{
-    data = fs.readFileSync("./html/main.html");
-    pageContent = data.toString();
-    response.send(pageContent);
+router.get("/", (request, response) => {
+  data = fs.readFileSync("./html/main.html");
+  pageContent = data.toString();
+  response.send(pageContent);
 });
-router.get('/home',(request,response)=>
-{
-    data = fs.readFileSync("./html/home.html");
-    pageContent = data.toString();
-    response.send(pageContent);
+router.get("/home", (request, response) => {
+  data = fs.readFileSync("./html/home.html");
+  pageContent = data.toString();
+  response.send(pageContent);
 });
-router.get('/about',(request,response)=>
-{
-    data = fs.readFileSync("./html/about.html");
-    pageContent = data.toString();
-    response.send(pageContent);
+router.get("/about", (request, response) => {
+  data = fs.readFileSync("./html/about.html");
+  pageContent = data.toString();
+  response.send(pageContent);
 });
-router.get('/contact',(request,response)=>
-{
-    data = fs.readFileSync("./html/contact.html");
-    pageContent = data.toString();
-    response.send(pageContent);
+router.get("/contact", (request, response) => {
+  data = fs.readFileSync("./html/contact.html");
+  pageContent = data.toString();
+  response.send(pageContent);
 });
 
 // -------------------------
-appserver.use('/', router);
-appserver.use('/home', router);
-appserver.use('/about', router);
-appserver.use('/contact', router);
-// ----------- RUN / Lauching !!! 
-appserver.listen( PORT,host );
+appserver.use("/", router);
+appserver.use("/home", router);
+appserver.use("/about", router);
+appserver.use("/contact", router);
+// ----------- RUN / Lauching !!!
+appserver.listen(PORT, host);
 
 console.log("Web da mo tai " + PORT);
