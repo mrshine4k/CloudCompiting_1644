8003115790220e90af0d5a42c0c76e7ac2010791
mrshine4k <111043449+mrshine4k@users.noreply.github.com>
2022-09-21 11:09:30 +0700

    s

--- appserver.js
+++ appserver.js
@@ -3,30 +3,40 @@ const PORT = process.env.PORT || 80;
 const appServer = express();
 const router = express.Router();
 const fs = require("fs");
+const { nextTick } = require("process");
 
-// ------------------- Routing
+//-------------------------------------------------middleware
+
+router.use('/',(yeucau, trave, tieptuc) => {
+    console.log('Time:', Date.now(), yeucau.url);
+    tieptuc();
+});
+
+// -------------------------------------------------- Routing
 router.get( "/" , (yeucau, trave) => {
-    data = fs.readFileSync("./html/main.html");
+    data = fs.readFileSync("./view/main.html");
     pageContent = data.toString();
     trave.send(pageContent);
 });
 
 router.get( "/home" , (yeucau, trave) => {
-    data = fs.readFileSync("./html/home.html");
+    data = fs.readFileSync("./view/home.html");
     pageContent = data.toString();
     trave.send(pageContent);
 });
 
 router.get( "/products" , (yeucau, trave) => {
-    data = fs.readFileSync("./html/products.html");
+    data = fs.readFileSync("./view/products.html");
     pageContent = data.toString();
     trave.send(pageContent);
 });
 
+// ---------------------------------------------use
 
-// -------------------------
 appServer.use("/", router);
+
 // ----------- RUN / Launching !!! 
+
 appServer.listen( PORT );
 
 console.log("Web da mo tai " + PORT);
